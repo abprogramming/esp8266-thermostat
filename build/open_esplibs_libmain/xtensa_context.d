08d5e35308e74375c623599507a3921dacb729dc
berces <berces@stp29>
2020-07-31 15:05:10 +0200

    remove build folder

--- build/open_esplibs_libmain/xtensa_context.d
+++
@@ -1,3 +0,0 @@
-build/open_esplibs_libmain//xtensa_context.o: \
- /media/berces/work1/esp/esp-open-rtos/open_esplibs/libmain/xtensa_context.S \
- ../../open_esplibs/include/open_esplibs.h
--- build/open_esplibs_libpp/lmac.d
+++
@@ -1,3 +0,0 @@
-build/open_esplibs_libpp//lmac.o: \
- /media/berces/work1/esp/esp-open-rtos/open_esplibs/libpp/lmac.c \
- ../../open_esplibs/include/open_esplibs.h
