59db3100ee9235b2343ce1d573d03bcb249e45af
berces <berces@stp29>
2020-08-06 16:40:07 +0200

    remove build folder

--- build/open_esplibs_libpp/esf_buf.d
+++
@@ -1,3 +0,0 @@
-build/open_esplibs_libpp//esf_buf.o: \
- /media/berces/work1/esp/esp-open-rtos/open_esplibs/libpp/esf_buf.c \
- ../../open_esplibs/include/open_esplibs.h
--- build/open_esplibs_libpp/wdev.d
+++
@@ -1,3 +0,0 @@
-build/open_esplibs_libpp//wdev.o: \
- /media/berces/work1/esp/esp-open-rtos/open_esplibs/libpp/wdev.c \
- ../../open_esplibs/include/open_esplibs.h
