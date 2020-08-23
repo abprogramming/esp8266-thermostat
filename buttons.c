d0a5913e1296b105928d3ac03a656898a1b020a8
adam <adam@adamb.catv.pool.telekom.hu>
2020-08-23 20:59:49 +0200

    Handling of the button callback, the timer and the ADC for the potentiometer

--- buttons.c
+++ buttons.c
@@ -23,115 +23,133 @@
 
 
 static TimerHandle_t display_timer = NULL;
-static TaskHandle_t set_temp_task_h = NULL;
+static TaskHandle_t  main_task_h = NULL;
+static TaskHandle_t  set_temp_task_h = NULL;
 static uint16_t last_adc_value = UINT16_MAX;
-static uint8_t set_mode = 0;
+static bool set_mode = false;
 
+
+/////////////////////////////////////////////////////
+// Temperature knob control and ADC voltage
+// conversion to temperature values
+
+static float celsius_per_adc_step = 
+    (TEMP_MAX - TEMP_MIN) / (ADC_MAX - ADC_MIN);
+    
 // Epsilon value to suppress noise, below this value
 // we consider the value is unchanged
-static const uint16_t adc_epsilon = 1;
+static const uint16_t adc_epsilon = 10;
 
 uint32_t adc_temp_to_uint32(uint16_t adc)
 {
-    uint32_t out = 0;
-    float temp;
-    //t16 to float 
-    out = FLT2UINT32(temp);
-    out <<= 16;
-    return out;
-}
-
-uint32_t get_notification_from_adc(uint16_t adc, uint16_t magic)
-{
-
+    float t = (ADC_MAX - adc) * celsius_per_adc_step;
+    t += TEMP_MIN;
+    return FLT2UINT32(t);
 }
 
 void set_temperature_task(void *pvParameters)
-{
-    TaskHandle_t main_task_h = (TaskHandle_t) pvParameters;
-    
-    // After starting the program and the task is created
-    // we read and store the actual value of the potentiometer
-    // to initialize last_adc_value and then suspend this task.
-    // It will be woken up button_interrupt. 
-    
+{    
     uint16_t adc = 0;
     uint32_t notify_val = 0;
     
     for (;;)
     {
         adc = sdk_system_adc_read();
+        dprintf("adc %u\n", adc);
         
-        if (adc == UINT16_MAX)
+        // After starting this task is created we read and store
+        // the actual value of the potentiometer to initialize
+        // last_adc_value and then suspend this task.
+        // It will be woken up button_interrupt. 
+        if (last_adc_value == UINT16_MAX)
         {
             last_adc_value = adc;
-            vTaskSuspend(set_temp_task_h);
+            vTaskSuspend(NULL);
         }
         
         if (abs(adc - last_adc_value) > adc_epsilon)
         {
-            set_mode = 1;
+            set_mode = true;
+            
             // Keep display alive
             xTimerReset(display_timer, 0);
+            
             notify_val   = adc_temp_to_uint32(adc);
-            // Append magic to notification value
             notify_val <<= 16;
             notify_val  += MAGIC_SET_TEMP;
+            
+            // Send temperature value to 
+            // main task for display
             xTaskNotify(main_task_h, notify_val,
                 (eNotifyAction) eSetValueWithOverwrite);   
+                
+            last_adc_value = adc;
         }
-        
         DELAY(100);
     }
-
 }
 
-void display_timer_cb(TimerHandle_t pxTimer)
-{
-    taskENTER_CRITICAL();
-    dprintf("timer end\n");
-    fflush(stdout);
-    // Suspend temperature knob control
-    vTaskSuspend(set_temp_task_h);
-    set_display_state(DISPLAY_OFF);
-    taskEXIT_CRITICAL();
-}
+
+/////////////////////////////////////////////////////
+// Temperature knob control and ADC voltage
+// conversion to temperature values
 
 void button_interrupt(uint8_t gpio_num)
 {
     (void)gpio_num;
-    
-    dprintf("GPIO ir\n");
-    
-    taskENTER_CRITICAL();
-    if (set_mode == 0)
-    {
-        set_display_state(DISPLAY_ON);
-        // Start temperature knob control
-        vTaskResume(set_temp_task_h);
-    }
-    else
-    {
-        // Send the final knob value 
-        // to the main for display and 
-        // setting the new temperature value
-        set_mode = 0;
+ 
+    // Disable this ISR until the display
+    // is turned off
+    gpio_set_interrupt(PIN_TACTILEBUTTON,
+        GPIO_INTTYPE_NONE, &button_interrupt);
         
-    }
-    taskEXIT_CRITICAL();
+    // Turn on display
+    set_display_state(DISPLAY_ON);
     
-    xTimerStart(display_timer, 10);
+    // Start temperature knob control
+    xTaskResumeFromISR(set_temp_task_h);
+    
+    // Start countdown to turn off display
+    // if there's no user interaction
+    xTimerStartFromISR(display_timer, (TickType_t) 10);
+ }
+ 
+void display_timer_cb(TimerHandle_t pxTimer)
+{   
+    // Suspend temperature knob control
+    vTaskSuspend(set_temp_task_h);
+    
+    // If a new temperature was set,
+    // send it for the main task
+    if (set_mode)
+    {
+        uint32_t notify_val = 0;
+        notify_val   = adc_temp_to_uint32(last_adc_value);
+        notify_val <<= 16;
+        notify_val  += MAGIC_ACC_TEMP;
+        set_mode = false;
+        xTaskNotify(main_task_h, notify_val,
+                (eNotifyAction) eSetValueWithOverwrite);   
+     }       
+    
+    // Turn off display
+    set_display_state(DISPLAY_OFF);
+       
+    // Re-enable interrupt to handle the
+    // button press event
+    gpio_set_interrupt(PIN_TACTILEBUTTON,
+        GPIO_INTTYPE_EDGE_POS, &button_interrupt);
 }
 
 void input_control_task(void *pvParameters)
 {
-    TaskHandle_t main_task_h = (TaskHandle_t) pvParameters;
+    main_task_h = (TaskHandle_t) pvParameters;
     
     gpio_enable(PIN_TACTILEBUTTON, GPIO_INPUT);
     
     // Attach an interrupt handler the GPIO pin of the tactile button
     gpio_set_interrupt(PIN_TACTILEBUTTON,
-        GPIO_INTTYPE_EDGE_ANY, &button_interrupt);
+        GPIO_INTTYPE_EDGE_POS, &button_interrupt);
             
     // Timer for the display, the callback function turns the
     // displaty to OFF state, if no user iteraction occurs in a time
