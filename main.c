06f41159c5b9eed55a689cf1f95f93d780bb00ca
adam <adam@adamb.catv.pool.telekom.hu>
2020-08-25 21:39:49 +0200

    Error checking of xTaskCreate

--- main.c
+++ main.c
@@ -19,32 +19,61 @@
  * the relay control (which is responsible to decide
  * when to turn on or off the heater).
  */
-
+ 
+// Task handles
+static TaskHandle_t main_task_h    = NULL;
+static TaskHandle_t temp_task_h    = NULL;
+static TaskHandle_t display_task_h = NULL;
+static TaskHandle_t input_task_h   = NULL;
+ 
 void user_init(void)
 {
     uart_set_baud(0, BAUDRATE);
-
+    
+    BaseType_t res;
+    
     // Main flow control
-    xTaskCreate(&main_task, "main_task",
-        256, NULL, PRIO_DEFAULT, &main_task_h);
-
-
+    res = xTaskCreate(&main_task, "main_task",
+            256, NULL, PRIO_DEFAULT, &main_task_h);
+    check_task_creation_result(res, "main");
+    
     // Each task receives the handle for the main task
 
     // Read temperature data from sensors
     xTaskCreate(&read_temp_task, "read_temp",
         256, (void*) main_task_h, PRIO_DEFAULT, &temp_task_h);
-
+    check_task_creation_result(res, "temperature sensor");
+    
     // Handle the 7-segment displays
     xTaskCreate(&display_control_task, "display_control",
         256, (void*) main_task_h, PRIO_DEFAULT, &display_task_h);
+    check_task_creation_result(res, "display control");
+        
     // Handle user input, button, switch and potentiometer
-    xTaskCreate(&input_control_task, "display_control",
+    xTaskCreate(&input_control_task, "input_task",
         256, (void*) main_task_h, PRIO_DEFAULT, &input_task_h);
+    check_task_creation_result(res, "input control");
     
     // Init relay module
     relay_init();
+    test_relay();
     set_relay_state(RELAY_OFF);
+}
+
+// If any of these critical calls above fails, we should
+// not continue and try to restart the MCU ASAP
+void check_task_creation_result(BaseType_t r, char *name)
+{
+    if (r != pdPASS)
+    {
+        dprintf("FATAL ERROR: failed to create %s task, "
+                "restarting system...\n", name);
+        fflush(stdout);
+        uart_flush_txfifo(0);
+        uart_flush_txfifo(1);
+        DELAY(10);
+        sdk_system_restart();
+    }
 }
 
 /////////////////////////////////////////////////////
@@ -58,6 +87,7 @@ void update_relay_state
 )
 {
     dprintf("rs act %u tgt %u h %u\n", actual, target, hyst);
+    
     if (*st == RELAY_OFF && 
         actual <= target - hyst)
     {
@@ -79,8 +109,6 @@ void main_task(void *pvParameters)
     uint16_t set_temp = (uint16_t) FLT2UINT32(TEMP_INITIAL);
     uint16_t hyst = (uint16_t) FLT2UINT32(HYST_INITIAL);
     relay_state_t relay_state = RELAY_OFF;
-
-    test_relay();
     
     // Wait some time to be sure everything is ready
     DELAY(3000);
@@ -120,3 +148,6 @@ void main_task(void *pvParameters)
         }
     }
 }
+
+
+
--- main.h
+++ main.h
@@ -1,13 +1,7 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-// Task handles
-static TaskHandle_t main_task_h    = NULL;
-static TaskHandle_t temp_task_h    = NULL;
-static TaskHandle_t display_task_h = NULL;
-static TaskHandle_t input_task_h   = NULL;
-
-void user_init(void);
 void main_task(void *pvParameters);
+void check_task_creation_result(BaseType_t r, char *name);
 
 #endif
