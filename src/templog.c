fd896b73e4abdacd822bffe965ab194a8ad8ade3
Bérces Ádám <36815060+abprogramming@users.noreply.github.com>
2020-10-17 21:31:09 +0200

    Remove unneccesary parenthesis

--- src/templog.c
+++ src/templog.c
@@ -3,7 +3,7 @@
 
 
 struct log_buffer_t log_buffer_init(size_t sz)
-{ 
+{
     struct log_buffer_t buf;
     buf.entries = sz;
     buf.first = calloc(buf.entries, sizeof(struct log_entry_t));
@@ -21,13 +21,13 @@ void log_buffer_push(struct log_buffer_t *buf, struct log_entry_t val)
 }
 
 struct log_entry_t log_buffer_getnext(struct log_buffer_t *buf)
-{      
+{
     if (buf->ptr_rd < buf->first)
     {
 		buf->ptr_rd = log_buffer_end(buf);
 		buf->ptr_rd--;
 	}
-    struct log_entry_t out = *(buf->ptr_rd);
+    struct log_entry_t out = *buf->ptr_rd;
     buf->ptr_rd--;
     return out;
 }
